67e65843d3888bde0a73b341c1dea822dcbd2de3
emmanuelprogramador242006 <emmanuelprogramador24@gmail.com>
2024-01-07 21:49:40 -0500

    Arreglos en php

---
+++ 09-arreglo.php
@@ -0,0 +1,12 @@
+<?php
+
+$lista = array("Banano", "platano", "piña", "mango");
+$lista2 = array("b" => "banano", "p" => "platano", "i" => "piña", "m" => "mango");
+
+print_r($lista);
+echo "<br>";
+print_r($lista2);
+
+
+
+?>
